e22c44f12472c74e3575b0ab013e468c3bf5a2d3
sumit_ra <sumit_ra@DS-SGH351SMTP.HCLT.CORP.HCL.IN>
2018-12-12 14:32:52 +0530

    Added properties for H2 Database

--- SpringBootDemo/src/main/java/com/microservices/Application.java
+++ SpringBootDemo/src/main/java/com/microservices/Application.java
@@ -2,8 +2,10 @@ package com.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class Application {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
